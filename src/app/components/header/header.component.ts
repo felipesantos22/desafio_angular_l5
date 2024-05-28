1388ea7955cf81cdd355197069432fc7bce74414
felipesantos22 <felipevs2018@gmail.com>
2024-05-28 15:08:33 -0300

    insert search

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  searchValue: string = '';
+
+  ngOnInit(): void {}
+
+  onSearch(value: string) {
+    console.log(value);
+    if (value && value.length > 3) {
+      this.router.navigate(['/'], {
+        queryParams: { q: value },
+      });
+    }
+  }
+}
