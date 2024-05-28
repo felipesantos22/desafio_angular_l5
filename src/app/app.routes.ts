c9ce8eeff5a86acc1389c06e2ae6c831b0f055bc
felipesantos22 <felipevs2018@gmail.com>
2024-05-28 14:04:12 -0300

    insert router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'details', component: DetailsComponent },
+];
--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import Character from '../../interfaces/character';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './character.component.html',
-  styleUrl: './character.component.css'
+  styleUrl: './character.component.css',
 })
 export class CharacterComponent {
-
+  @Input() data: Character[] = [];
 }
