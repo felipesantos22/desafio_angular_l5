9ce881fdcc9938e6e9aee7317684d083a0b2e3f0
felipesantos22 <felipevs2018@gmail.com>
2024-05-28 15:52:48 -0300

    change interface character

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,22 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import Character from '../interfaces/character';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'https://rickandmortyapi.com/api/character';
+  private apiUrl = 'https://rickandmortyapi.com/api/character/';
 
   constructor(private http: HttpClient) {}
 
-  getCharacters(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+  searchCharacters(query = '', page = 1) {
+    const url = `${this.apiUrl}/?name=${query}&page=${page}`;
+    return this.http.get<Character[]>(url);
   }
 
-  // getCharacters(): Observable<any> {
-  //   const url = `${this.apiUrl}`;
-  //   return this.http.get(url);
-  // }
+  getDetails(id: Number) {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Character>(url);
+  }
 
+  getCharacters(): Observable<any> {
+    const url = `${this.apiUrl}`;
+    return this.http.get(url);
+  }
 }
